a18cb4e5abe63857f6671d8caf7a684a462e2586
Juan Alberto Farias <dientuki@elizabeth.(none)>
2010-04-14 13:07:10 -0300

    eliminada basura

--- wp/wp-content/plugins/sitepress-multilingual-cms/inc/compatibility-packages/init-packages.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($sitepress_settings['existing_content_language_verified']){
-    $WPML_Packages = new WPML_Packages();
-}
-?>
--- wp/wp-content/plugins/sitepress-multilingual-cms/res/js/translation-synchronization.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(){
-    jQuery('#icl_page_sync_options').submit(iclSaveForm);    
-    jQuery('form[name="icl_plugins_texts"]').submit(iclSaveForm);
-})
