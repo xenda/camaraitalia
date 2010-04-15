fe4aaa4b15e9230186b15f5e3abd5d9efd97d03d
Juan Alberto Farias <dientuki@elizabeth.(none)>
2010-04-15 07:38:49 -0300

    languaje selector beta

--- wp/wp-content/themes/camara/functions.php
+++ wp/wp-content/themes/camara/functions.php
@@ -37,3 +37,12 @@ function load_js($theme_url) {
 		echo '<script type="text/javascript" src="'. $theme_url . '/js/scripts.js"></script>';	
 	}
 }
+
+if ( function_exists('register_sidebar') ) {
+	register_sidebar(array(
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h2 class="widgettitle">',
+		'after_title' => '</h2>',
+	));
+}
